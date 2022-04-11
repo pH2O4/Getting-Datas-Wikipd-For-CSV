36838f1fe345c609a0d5dfb5f0f6ccf2bee9d78d
pH2O4 <ph2oalencar@gmail.com>
2022-04-10 21:27:58 -0300

    -

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const puppeteer = require('puppeteer');
 const ExcelJS = require('exceljs')
-
-const workbook = new ExcelJS.Workbook()
-const sheet = workbook.addWorksheet('Firts Pag')
-
+        const workbook = new ExcelJS.Workbook()
+        const sheet = workbook.addWorksheet('Firts Pag')
 const myDatasTrated = () => {
     (async () => {
         const browser = await puppeteer.launch({ headless: false });
@@ -17,32 +15,40 @@ const myDatasTrated = () => {
 
             const nodeListTR = [document.querySelectorAll("tbody")]
             const nodeListTRTable = nodeListTR[0]
-            const nodeListTRTBOdyTable = nodeListTRTable[1]
+            const nodeListTRTBOdyTable = [nodeListTRTable[1].children]
+            const ListingTR = nodeListTRTBOdyTable[0]
 
-            const myDatasTrated = [nodeLIstTHITEM0, nodeListTRTBOdyTable]
-            console.log(nodeLIstTHITEM0, nodeListTRTBOdyTable)
+            const myDatasTrated = [nodeLIstTHITEM0, ListingTR]
+            console.log(nodeLIstTHITEM0[1]., ListingTR[1])
             return myDatasTrated
         })
 
-        for (let index = 0; index < myDatasTrated[0].length; index++) {
+
+        try {
+          for (var index = 0; index < myDatasTrated[0].length; index++) {
             const ArrayselectedTH = myDatasTrated[0]
-            const curtingArray = ArrayselectedTH[index].textContent
-            sheet.columns = [
-                { header: `${curtingArray}`, hey: `${index}Colum` },
-            ]
+            const curtingArrayTH = ArrayselectedTH[index].value
+            sheet.columns = [{ header:`${curtingArrayTH}`, key: `${index}` }]
         }
         for (let index2 = 0; index2 < myDatasTrated[1].length; index2++) {
             const ArrayselectedTR = myDatasTrated[1]
-            const curtingArray = ArrayselectedTH[index2].textContent
+            const curtingArrayTR = ArrayselectedTR[index2].children.value
+            console.log(curtingArrayTR)
             sheet.addRow({
-                nome
+                index: curtingArrayTR
             })
+        }  
+        } catch (error) {
+            console.log('asa')
         }
+        
 
-        //  await browser.close()
+
+        sheet.workbook.xlsx.writeFile('texsadf.xlsx')
+
+       //  await browser.close()
     })()
 }
 
 myDatasTrated()
 
-module.exports = myDatasTrated

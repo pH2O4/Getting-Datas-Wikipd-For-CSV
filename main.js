3ec8e982689c87fa34967c2490fb82369d731dc4
pH2O4 <ph2oalencar@gmail.com>
2022-04-11 12:35:46 -0300

    ---sorry but i give upp this project, the code function for a moment and after don't function

--- main.js
+++ main.js
@@ -3,23 +3,24 @@ const ExcelJS = require('exceljs')
         const workbook = new ExcelJS.Workbook()
         const sheet = workbook.addWorksheet('Firts Pag')
 const myDatasTrated = () => {
+    
     (async () => {
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto('https://en.wikipedia.org/wiki/Road_safety_in_Europe');
 
-        const myDatasTrated = await page.evaluate(() => {
+        const myDatasTrated =  page.evaluate(() => {
             const nodeListTH = [document.getElementsByClassName("headerSort")]
             const nodeLIstTHITEM0 = nodeListTH[0]
 
 
-            const nodeListTR = [document.querySelectorAll("tbody")]
-            const nodeListTRTable = nodeListTR[0]
-            const nodeListTRTBOdyTable = [nodeListTRTable[1].children]
-            const ListingTR = nodeListTRTBOdyTable[0]
+            const nodeListTR =  [document.querySelectorAll("tbody")]
+            const nodeListTRTable =  nodeListTR[0]
+            const nodeListTRTBOdyTable = [nodeListTRTable[1]]
+            const ListingTR =  nodeListTRTBOdyTable[0].children
 
             const myDatasTrated = [nodeLIstTHITEM0, ListingTR]
-            console.log(nodeLIstTHITEM0[1]., ListingTR[1])
+            console.log(nodeLIstTHITEM0.item(0), ListingTR.item(0))
             return myDatasTrated
         })
 
@@ -27,13 +28,12 @@ const myDatasTrated = () => {
         try {
           for (var index = 0; index < myDatasTrated[0].length; index++) {
             const ArrayselectedTH = myDatasTrated[0]
-            const curtingArrayTH = ArrayselectedTH[index].value
+            const curtingArrayTH = ArrayselectedTH[index]
             sheet.columns = [{ header:`${curtingArrayTH}`, key: `${index}` }]
         }
         for (let index2 = 0; index2 < myDatasTrated[1].length; index2++) {
             const ArrayselectedTR = myDatasTrated[1]
-            const curtingArrayTR = ArrayselectedTR[index2].children.value
-            console.log(curtingArrayTR)
+            const curtingArrayTR = ArrayselectedTR[index2]
             sheet.addRow({
                 index: curtingArrayTR
             })
@@ -44,7 +44,7 @@ const myDatasTrated = () => {
         
 
 
-        sheet.workbook.xlsx.writeFile('texsadf.xlsx')
+        sheet.workbook.xlsx.writeFile('VAI DAR .xlsx')
 
        //  await browser.close()
     })()
